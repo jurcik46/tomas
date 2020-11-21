17e395b1b1a4769f4bc346d505b9abb0d95d9256
jan.jurc@brainit.sk <9OUnMDsLB4S6w6fZi>
2020-11-21 19:01:32 +0100

    fix

--- db.js
+++ db.js
@@ -1,3 +1,12 @@
 var mysql = require('mysql')
 var dbConfig = require("./config.js");
 var connection = mysql.createConnection(dbConfig.db);
+
+function execute(query, params, result){
+    connection.connect();
+    connection.query(query ,params, result);
+    connection.end();
+}
+
+
+module.exports = {execute}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var homepageController = require("../controller/homepage")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', homepageController.res());
 });
 
 module.exports = router;
